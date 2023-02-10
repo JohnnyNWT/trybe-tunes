fa868f4366a9af7a09c1afe9de580588e6d373fd
JohnnyNWT <jooaaooskt@gmail.com>
2023-02-10 10:53:18 -0300

    FEAT: implementa esqueleto da pagina de profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import Header from '../components/Header';
+import ProfileIcon from '../images/profile-icon1.png';
+import { getUser } from '../services/userAPI';
 
 export default class Profile extends React.Component {
+  state = {
+    username: '',
+  };
+
+  async componentDidMount() {
+    const user = await getUser();
+    this.setState({
+      username: user.name,
+    });
+  }
+
   render() {
+    const { username } = this.state;
     return (
       <>
         <Header />
-        <div data-testid="page-profile">Profile</div>
+        <img src={ ProfileIcon } alt="img  profile" />
+        <div>
+          <h5>Nome</h5>
+          <p>{ username }</p>
+          <h5>E-mail</h5>
+          <p>usuario@usuario.com.br</p>
+          <h5>Descrição</h5>
+          <p>blablabla</p>
+        </div>
       </>
     );
   }
